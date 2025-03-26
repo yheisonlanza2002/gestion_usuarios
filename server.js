8f64879bc8b0f02db05bd81ceb2b5228dee6cbb0
Neicy Ortiz <yheison lanza>
2025-03-25 22:58:04 -0500

    confirmando

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('server/db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`JSON Server running on http://localhost:${PORT}`);
+});
